971bd928dd36ca057f5e46aead928fc5c5eeb062
Danila <golubev.d.a@mail.ru>
2022-12-27 08:18:16 +0300

    step two

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
 const program = new Command();
 
 program
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
