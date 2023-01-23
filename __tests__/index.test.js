86446c12e86c41000578afb0b0e8f30b63507b12
Danila <golubev.d.a@mail.ru>
2023-01-23 10:12:28 +0300

    updated test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,30 +2,20 @@ import { expect, test } from '@jest/globals';
 import genDiff from '../index.js';
 import readFile from '../src/readfile.js';
 
-const testFile = readFile('test.txt');
-const testPlain = readFile('testPlain.txt');
-const testJson = readFile('testJSON.txt');
-
-test('stylish gendiff.json', () => {
+test('stylish gendiff.json and .yml', () => {
+  const testFile = readFile('test.txt');
   expect(genDiff(('file1.json'), ('file2.json'))).toEqual(testFile);
-});
-
-test('stylish gendiff.yml', () => {
   expect(genDiff(('file1.yml'), ('file2.yml'))).toEqual(testFile);
 });
 
-test('plain gendiff.json', () => {
+test('plain gendiff.json and .yml', () => {
+  const testPlain = readFile('testPlain.txt');
   expect(genDiff(('file1.json'), ('file2.json'), 'plain')).toEqual(testPlain);
-});
-
-test('plain gendiff.yml', () => {
   expect(genDiff(('file1.yml'), ('file2.yml'), 'plain')).toEqual(testPlain);
 });
 
-test('json gendiff.json', () => {
+test('json gendiff.json and .yml', () => {
+  const testJson = readFile('testJSON.txt');
   expect(genDiff(('file1.json'), ('file2.json'), 'json')).toEqual(testJson);
-});
-
-test('json gendiff.yml', () => {
   expect(genDiff(('file1.yml'), ('file2.yml'), 'json')).toEqual(testJson);
 });
